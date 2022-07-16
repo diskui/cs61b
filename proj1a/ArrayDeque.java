684e393278bcfad4259a35bb94439b9603adb887
windows <kaxiford@gmail.com>
2022-07-16 18:34:49 +0800

    proj1a eighth try, imporve the style, fix bug of shrink

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -42,7 +42,9 @@ public class ArrayDeque<T> {
      */
     private void grow() {
         T[] newArray = (T[]) new Object[length * 2];
-        for (int i = (nextFirst + 1) % length, j = 0; j < size; j += 1, i = (i + 1) % length) {
+        for (int i = (nextFirst + 1) % length, j = 0;
+             j < size;
+             j += 1, i = (i + 1) % length) {
             newArray[j] = items[i];
         }
         length *= 2;
@@ -68,7 +70,7 @@ public class ArrayDeque<T> {
     }
 
     /**
-     * add an item of type T to the end of the deque
+     * add an item of type T to the end of the deque.
      *
      * @param item item
      */
@@ -83,25 +85,27 @@ public class ArrayDeque<T> {
     }
 
     /**
-     * return whether the deque is empty
+     * return whether the deque is empty.
      */
     public boolean isEmpty() {
         return size == 0;
     }
 
     /**
-     * return the number of items in deque
+     * return the number of items in deque.
      */
     public int size() {
         return size;
     }
 
     /**
-     * shrink the array to the size of it's half
+     shrink the array to the size of it's half.
      */
     private void shrink() {
         T[] newArray = (T[]) new Object[length / 2];
-        for (int i = (nextFirst + 1) % length, j = 0; j < size; j += 1, i += 1) {
+        for (int i = (nextFirst + 1) % length, j = 0;
+             j < size;
+             j += 1, i = (i + 1) % length) {
             newArray[j] = items[i];
         }
         length /= 2;
@@ -112,7 +116,8 @@ public class ArrayDeque<T> {
     }
 
     /**
-     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
+     * Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.
      */
     public T removeFirst() {
         if (isEmpty()) {
@@ -127,7 +132,8 @@ public class ArrayDeque<T> {
     }
 
     /**
-     * Removes and returns the item at the back of the deque. If no such item exists, returns null.
+     * Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null.
      */
     public T removeLast() {
         if (isEmpty()) {
@@ -143,21 +149,26 @@ public class ArrayDeque<T> {
 
 
     /**
-     * Gets the item at the given index, where 0 is the front, 1 is the next item,
+     * Gets the item at the given index,
+     * where 0 is the front, 1 is the next item,
      * and so forth. If no such item exists, returns null.
+     *
+     * @param index index
      */
     public T get(int index) {
-        if(index >= size) {
+        if (index >= size) {
             return null;
         }
         return items[(nextFirst + index + 1) % length];
     }
 
     /**
-     * print the entire deque from front to end
+     * print the entire deque from front to end.
      */
     public void printDeque() {
-        for (int i = (nextFirst + 1) % length; i != nextLast; i = (i + 1) % length) {
+        for (int i = (nextFirst + 1) % length;
+             i != nextLast;
+             i = (i + 1) % length) {
             System.out.print(items[i] + " ");
         }
     }
