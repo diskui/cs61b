64ca4ce027313c5661e3620a77d6596b171c3d45
windows <kaxiford@gmail.com>
2022-07-25 01:15:09 +0800

    lab 11 first try

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -10,20 +13,63 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+
+    private int t;
+
+    private boolean targetFound = false;
+
+    private Maze maze;
+
+    private Queue<Integer> queue;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
+        maze = m;
+
+        queue = new Queue<Integer>();
+
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+
+
         // Add more variables here!
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[v] = true;
+        announce();
+
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+        for(int w : maze.adj(v)){
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                queue.enqueue(w);
+            }
+        }
+        int next = queue.dequeue();
+        bfs(next);
+
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -10,13 +10,38 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private Maze maze;
+
+    private int[] nodeTo;
+
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        nodeTo = new int[maze.N() * maze.N()];
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(0);
+    }
+
+    /* its parent, it */
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                nodeTo[w] = v;
+                dfs(w);
+            }else{
+                if(nodeTo[v] != w){
+                    edgeTo[v] = w;
+                    return;
+                }
+            }
+        }
     }
 
     // Helper methods go here
